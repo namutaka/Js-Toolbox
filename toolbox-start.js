1711eb99af4851a02238bed04d813c83af1390da
namutaka <namutaka+g@gmail.com>
2012-12-22 15:10:33 +0900

    JSだけで簡潔できるようにrequirejsを導入

--- toolbox-start.js
+++ toolbox-start.js
@@ -1,12 +1,12 @@
-(function(s) {
-  var htmlText='<html><head></head><body>' +
-'<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>' +
-'<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.19/jquery-ui.min.js"></script>' +
-'<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.19/themes/base/jquery-ui.css">' +
-'<script type="text/javascript" charset="utf-8" src="'+s+'"></script>' +
-'<script>var app; setTimeout(function() {app = new Toolbox.app()},500); </script>' +
-'</body></html>';
-var win = window.open('about:blank', 'TB',
-  'width=320,height=150,menubar=no,toolbar=no,scrollbars=yes');
-win.document.write(htmlText);
-})('http://localhost:9393/toolbox.js?z');
+(function() {
+  var src = $('script[src*="toolbox-start.js"]').attr('src');
+  var js_src = src.replace('toolbox-start', 'toolbox');
+  var htmlText='<html><head>' +
+    '<script type="text/javascript" charset="utf-8"' +
+      ' src="//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.1/require.min.js"' +
+      ' data-main="' + js_src+'?v=1"></script>' +
+    '</head><body></body></html>';
+  var win = window.open('about:blank', 'TB',
+    'width=320,height=350,menubar=no,toolbar=no,scrollbars=yes');
+  win.document.write(htmlText);
+})();
--- toolbox.js
+++ toolbox.js
@@ -1,104 +1,119 @@
-/**
- * toolbox
- */
-var Toolbox = {};
+require([
+  'https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js',
+  'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.19/jquery-ui.min.js'
+], function(JQuery) {
+  var Toolbox = function() {
+    this.body = $('body');
+    this.opener = window.opener;
 
-Toolbox.app = function() {
-  this.body = $('body');
-  this.opener = window.opener;
+    this.setupGui();
+    this.onOpenerLoad();
+  };
 
-  this.setupGui();
-  this.onOpenerLoad();
-};
+  Toolbox.prototype = {
+    setupEvent: function() {
+      var self = this;
+      $(this.opener).unload(function() {
+        var polling;
+        polling = function () {
+          if (self.opener.document.readyState == "complete") {
+            self.onOpenerLoad();
+          } else {
+            setTimeout(polling, 100);
+          }
+        };
+        setTimeout(polling, 100);
+      });
+    },
 
-Toolbox.app.prototype = {
-  setupEvent: function() {
-    var self = this;
-    $(this.opener).unload(function() {
-      var polling;
-      polling = function () {
-        if (self.opener.document.readyState == "complete") {
-          self.onOpenerLoad();
-        } else {
-          setTimeout(polling, 100);
-        }
-      };
-      setTimeout(polling, 100);
-    });
-  },
+    setupGui: function (){
+      var self = this;
+      this.body.css('font-size', '12px');
 
-  setupGui: function (){
-    var self = this;
-    this.body.css('font-size', '12px');
+      var $buttons = $('<div/>').buttonset()
+        .addClass('ui-widget-header')
+        .addClass('ui-corner-all');
+      this.body.append($buttons);
 
-    var $buttons = $('<div/>').buttonset()
-      .addClass('ui-widget-header')
-      .addClass('ui-corner-all');
-    this.body.append($buttons);
+      $buttons.append(
+        $('<button/>')
+          .text('refresh')
+          .button({icons: {primary: "ui-icon-refresh"}})
+          .click(function() {
+            self.refresh();
+        })
+      );
 
-    $buttons.append(
-      $('<button/>')
-        .text('refresh')
-        .button({icons: {primary: "ui-icon-refresh"}})
-        .click(function() {
-          self.refresh();
-      })
-    );
+      $buttons.append(
+        $('<button/>')
+          .text('Show name')
+          .button()
+          .click(function() {
+            self.showName();
+        })
+      );
 
-    $buttons.append(
-      $('<button/>')
-        .text('Show name')
-        .button()
-        .click(function() {
-          self.showName();
-      })
-    );
+      this.body.append(
+        $('<textarea/>').width(300).height(120)
+      );
+    },
 
-    this.body.append(
-      $('<textarea/>').width(300).height(120)
-    );
-  },
+    onOpenerLoad: function() {
+      this.refresh();
+      this.setupEvent();
+    },
 
-  onOpenerLoad: function() {
-    this.refresh();
-    this.setupEvent();
-  },
+    refresh: function() {
+      $('textarea').val(
+          $.stringify(this.opener.value));
+    },
 
-  refresh: function() {
-    $('textarea').val(
-        $.stringify(this.opener.value));
-  },
-
-  showName: function() {
-    this.opener.$('input').each(function() {
-      $(this).after($('<span/>').text($(this).attr('name')));
-    });
-  }
-};
-
-
-
-jQuery.extend({
-    stringify : function stringify(obj) {
-        var t = typeof (obj);
-        if (t != "object" || obj === null) {
-            // simple data type
-            if (t == "string") obj = '"' + obj + '"';
-            return String(obj);
-        } else {
-            // recurse array or object
-            var n, v, json = [], arr = (obj && obj.constructor == Array);
-
-            for (n in obj) {
-                v = obj[n];
-                t = typeof(v);
-                if (obj.hasOwnProperty(n)) {
-                    if (t == "string") v = '"' + v + '"'; else if (t == "object" && v !== null) v = jQuery.stringify(v);
-                    json.push((arr ? "" : '"' + n + '":') + String(v));
-                }
-            }
-            return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
-        }
+    showName: function() {
+      this.opener.$('input').each(function() {
+        $(this).after($('<span/>').text($(this).attr('name')));
+      });
     }
+  };
+
+
+
+  jQuery.extend({
+      stringify : function stringify(obj) {
+          var t = typeof (obj);
+          if (t != "object" || obj === null) {
+              // simple data type
+              if (t == "string") obj = '"' + obj + '"';
+              return String(obj);
+          } else {
+              // recurse array or object
+              var n, v, json = [], arr = (obj && obj.constructor == Array);
+
+              for (n in obj) {
+                  v = obj[n];
+                  t = typeof(v);
+                  if (obj.hasOwnProperty(n)) {
+                      if (t == "string") v = '"' + v + '"'; else if (t == "object" && v !== null) v = jQuery.stringify(v);
+                      json.push((arr ? "" : '"' + n + '":') + String(v));
+                  }
+              }
+              return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+          }
+      }
+  });
+
+  new Toolbox();
 });
+
+//loadCss
+(function (url_list) {
+  for(var i = 0; i < url_list.length; i++) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url_list[i];
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }
+})([
+  'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.19/themes/base/jquery-ui.css'
+]);
 
